7b1dfb604cbdd00b9b3bbc932540166b18dbd352
Kaustuva Ghosh <ghosh.kaustuva@gmail.com>
2019-05-02 07:57:11 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app =express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname+"/public"));
+
+app.get('/',function(req,res) {
+  res.render('index.ejs');
+});
+app.get('/contact',function(req,res) {
+  res.render('contact.ejs');
+});
+app.get('/onside',function(req,res) {
+  res.render('onside.ejs');
+});
+app.get('/skills',function(req,res) {
+  res.render('skills.ejs');
+});
+app.get('/about',function(req,res) {
+  res.render('about.ejs');
+});
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(req,res){
+  console.log('listening on'+process.env.PORT);
+});
